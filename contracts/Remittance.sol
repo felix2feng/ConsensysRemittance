253b19c4d5f946d9e97dda43fed43b1a66511746
Felix <felix2feng@gmail.com>
2017-09-15 17:55:25 -0700

    Add additional logic to ensure non-doubly usage of passcodes

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -24,9 +24,12 @@ contract Remittance is Owned {
         address recipient;
         uint balance;
         uint deadline; // Deadline in block number
+        bytes32 passCode1;
+        bytes32 passCode2;
     }
     
     mapping(bytes32 => Remitter) public remittances;
+    mapping(bytes32 => bool) public usedPasscodes;
     
     function Remittance() {
         // Deploying contract costs 543827 gas
@@ -34,7 +37,7 @@ contract Remittance is Owned {
     }
     
     // @param passCode The hash of two hashed inputs
-    function deposit(bytes32 passCode, uint deadline, address recipient) 
+    function deposit(bytes32 hashCode1, bytes32 hashCode2, uint deadline, address recipient) 
         payable
         returns (bool)
     {
@@ -46,33 +49,50 @@ contract Remittance is Owned {
         require(deadline != 0);
         require(deadline >= dayInBlocks && deadline <= weekInBlocks);
         require(passCode != 0);
+
+        bytes32 doublyHashedPasscode = hashTwoInputs(hashCode1, hashCode2);
         
         // Require that the passcode is unique and not a reuse
         // We know it has been used if a previous owner is set
         require(remittances[passCode].owner == 0);
+
+        // Require that previous indiividual passcodes have not been used
+        require(usedPasscodes[hashCode1] == 0);
+        require(usedPasscodes[hashCode2] == 0);
         
         remittances[passCode].owner = msg.sender;
         remittances[passCode].recipient = recipient;
         remittances[passCode].balance = msg.value;
         remittances[passCode].deadline = block.number + deadline;
+
+        // Log used passcodes so they cannot be used again
+        usedPassCodes[hashCode1] = true;
+        usedPassCodes[hashCode2] = true;
         
         LogDeposit(msg.sender, recipient, msg.value, passCode);
         
         return true;
     }
     
-    function withdraw(bytes32 hash1, bytes32 hash2) 
+    function withdraw(string passcode1, string passcode2) 
         public
         returns (bool)
     {
-        var passCodeAttempt = hashTwoInputs(hash1, hash2);
+        bytes32 hashedCode1 = hashOneInput(passcode1);
+        bytes32 hashedCode2 = hashOneInput(passcode2);
+
+        bytes32 remittanceLocation = hashTwoInputs(hashedCode1, hashedCode2);
+
+        require(remittances[remittanceLocation].passCode1 == hashedCode1);
+        require(remittances[remittanceLocation].passCode2 == hashedCode2);
+
         // There must be a balance in the hashed figure
-        uint currentBalance = remittances[passCodeAttempt].balance;
+        uint currentBalance = remittances[remittanceLocation].balance;
         
         // There must be a balance in the hashed result
         require(currentBalance != 0);
         
-        uint deadlineBlock = remittances[passCodeAttempt].deadline;
+        uint deadlineBlock = remittances[remittanceLocation].deadline;
         
         if (owner == msg.sender) {
             // If the sender is the owner, he can only withdraw after deadline
@@ -85,7 +105,7 @@ contract Remittance is Owned {
             require(block.number <= deadlineBlock);    
         }
         
-        remittances[passCodeAttempt].balance = 0;
+        remittances[remittanceLocation].balance = 0;
         
         msg.sender.transfer(currentBalance - fee);
         
